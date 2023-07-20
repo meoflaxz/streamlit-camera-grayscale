c4d20d419d08da5aca876d8551c1cd3164567f04
Meor Hasyim <meorhashim@gmail.com>
2023-07-20 19:54:01 +0800

    add image upload feature

--- venv/cam.py
+++ venv/cam.py
@@ -1,17 +1,26 @@
 import streamlit as st
 from PIL import Image
 
-# Start camera
-with st.expander("Start Camera"):
-    camera_image = st.camera_input("Camera")
 
-if camera_image:
-    # Create a pillow image instance
-    img = Image.open(camera_image)
-
-    # Convert the pillow image to grayscale
-    # L is single channel aka luminance
+def parse(value):
+    img = Image.open(value)
     gray_img = img.convert("L")
-
-    # Render grayscale image in webpage
     st.image(gray_img)
+
+
+def main():
+    st.title("Image Processing App")
+    uploaded_image = st.file_uploader("Upload Image")
+
+    # Start camera
+    with st.expander("Start Camera"):
+        camera_image = st.camera_input("Camera")
+
+    if camera_image:
+        parse(camera_image)
+
+    if uploaded_image:
+        parse(uploaded_image)
+
+if __name__ == "__main__":
+    main()
